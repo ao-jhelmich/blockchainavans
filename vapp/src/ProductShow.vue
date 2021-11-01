bbd231cc5f9aec9d2e3a727facab2b55a57d3085
Jasper Helmich <jasper.helmich@gmail.com>
2021-11-01 18:31:54 +0100

    wip

--- vapp/src/ProductShow.vue
+++ vapp/src/ProductShow.vue
@@ -4,18 +4,23 @@
     <h3 class="text-lg leading-6 font-medium text-gray-900">
       Product Informatie ({{ barcode }})
     </h3>
-    <p class="mt-1 max-w-2xl text-sm text-gray-500">
+
+    <div v-if="error" v-text="error" class="bg-pink-100 text-pink-500 p-4 mt-5 border border-pink-500">
+
+    </div>
+
+    <p v-else class="mt-1 max-w-2xl text-sm text-gray-500">
       Omschrijving, locatie en barcode
     </p>
   </div>
   <div class="border-t border-gray-200">
-    <dl>
+    <dl v-if="product">
       <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
         <dt class="text-sm font-medium text-gray-500">
           Omschrijving
         </dt>
         <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-          {{ name }}
+          {{ product.name }}
         </dd>
       </div>
       <div class="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
@@ -23,7 +28,7 @@
           Locatie
         </dt>
         <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-          {{ location }}
+          {{ product.location }}
         </dd>
       </div>
       <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
@@ -31,7 +36,7 @@
           Barcode
         </dt>
         <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-          {{ barcode }}
+          {{ product.barcode }}
         </dd>
       </div>
     </dl>
@@ -45,8 +50,8 @@ export default {
   name: 'ProductShow',
   data() {
     return {
-      name: null,
-      location: null,
+      error: null,
+      product: null,
     }
   },
 
@@ -59,14 +64,13 @@ export default {
   },
 
   async created() {
-      this.name = await this.drizzleInstance.contracts.ProductContract.methods
-        .getName(this.barcode)
-        .call();
-
-      this.location = await this.drizzleInstance.contracts.ProductContract.methods
-        .getLocation(this.barcode)
-        .call();
-
+      try {
+        this.product = await this.drizzleInstance.contracts.ProductContract.methods
+          .getProduct(this.barcode)
+          .call();
+      } catch (error) {
+        this.error = '[FOUTE BESTEMMING] Boete toegediend, check je boete overzicht'
+      }
   }
 }
 </script>
