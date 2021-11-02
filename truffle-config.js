7dd115ef5eb13416522c9b4082a6b9ee4d5f3ef3
Jasper Helmich <jasper.helmich@gmail.com>
2021-11-02 11:19:15 +0100

    wip

--- truffle-config.js
+++ truffle-config.js
@@ -1,4 +1,5 @@
 const path = require('path')
+
 module.exports = {
   contracts_build_directory: path.join(__dirname, "vapp/src/contracts"),
 
@@ -7,10 +8,6 @@ module.exports = {
       host: "127.0.0.1",
       port: 7545,
       network_id: "*" // Match any network id
-    },
-    // besuWallet: {
-    //   provider: privateKeyProvider,
-    //   network_id: "*"
-    // },
+    }
   }
 };
